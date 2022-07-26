96c83169a4a3131fb9e41d58547ed2c2bddbd3cb
mariam-hamdy <62889447+mariam-hamdy@users.noreply.github.com>
2022-07-26 18:17:58 +0200

    Add files via upload

--- express.js
+++ express.js
@@ -1,13 +1,21 @@
 const { response } = require('express')
+const products = require('./data')
 const express = require('express')
 const app = express()
 
-app.use(express.static('./public'))
 
-/*app.get('/', (request, response) => {
-    response.status(200).send('<h1>Home Page</h1>')
+
+//app.use(express.static('./public'))
+
+app.get('/', (request, response) => {
+    //I write it as array of objects then it is converted to json
+    let newProducts = products.map((product) => {
+        let {id,name,price} = product
+        return {id,name,price}
+    })
+    response.json(newProducts)
 })
-app.get('/about', (request, response) => {
+/*app.get('/about', (request, response) => {
     response.status(200).send('<h1>About Page</h1>')
 })*/
 app.all('*', (request,response) => {
